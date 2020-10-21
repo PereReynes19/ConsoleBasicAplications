ed48289c0172d61705d564e806741dc44cbb354b
Prova <prova@gmail.com>
2020-10-21 11:18:01 +0200

    Created exercise 2, created a list.
    Created a new string List with obejects.

---
+++ ConsoleBasicAplications_ListObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleBasicAplications_ListObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nombres = new List<String>() {"Marti Ripoll", "Pere Boladeras", "Stoik Axondo"};
+
+            foreach (String nombre in nombres)
+            {
+                Console.WriteLine(nombre);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
