448bed240b3379c80338d9bc13812ef4a1b656c5
Prova <prova@gmail.com>
2020-10-21 11:03:16 +0200

    Exercise 1 list of Strings finished.
    Added a foreach loop to read the content of the array of Strings.
    Added a thread sleeper to pause the content and to see it in the console.

---
+++ ConsoleBasicAplications_StringsList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleBasicAplications_StringsList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nombres = new String[] {"Juan Rodriguez", "Pere Reynes", "Maria Garcia"};
+
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine(nombre);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
